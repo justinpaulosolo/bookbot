0dbfc20ae22d39554bd160ffb904a2356e190821
Justin Solo <justinpaulosolo@gmail.com>
2024-07-26 12:29:35 -0700

    add print report function

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     char_dict = get_chars_dict(text)
+    print_report(char_dict, word_count)
 
-    print(char_dict)
+def print_report(char_dict, word_count):
+    sorted_dict = dict(sorted(char_dict.items(), key=lambda item: item[1], reverse=True))
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words foun in the document")
 
+    print()
+    for char in sorted_dict:
+        if char.isalpha():
+            print(f"The '{char}' character was found {sorted_dict[char]} times")
+
+    print("--- End report ---")
 
 def get_chars_dict(text):
     chars = {}
