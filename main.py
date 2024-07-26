d995d7300829017bbdc96bff3ff97aa2cefb1be9
Justin Solo <justinpaulosolo@gmail.com>
2024-07-26 11:58:40 -0700

    add get_chars_dict function

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
-
     word_count = get_word_count(text)
+    char_dict = get_chars_dict(text)
 
-    print(word_count)
+    print(char_dict)
+
+
+def get_chars_dict(text):
+    chars = {}
+
+    for char in text:
+        lowered_char= char.lower()
+        if lowered_char in chars.keys():
+            chars[lowered_char] += 1
+        else:
+            chars[lowered_char] = 1
+
+    return chars
 
 
 def get_book_text(path):
